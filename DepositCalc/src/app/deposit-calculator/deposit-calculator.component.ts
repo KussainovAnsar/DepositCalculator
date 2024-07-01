6f432c694e81c390227608c82ce47a69dfd60dfa
KussainovAnsr <Ansar02012004@gmail.com>
2024-07-01 11:55:44 +0500

    Initial commit

---
+++ DepositCalc/src/app/deposit-calculator/deposit-calculator.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-deposit-calculator',
+  standalone: true,
+  imports: [],
+  templateUrl: './deposit-calculator.component.html',
+  styleUrl: './deposit-calculator.component.scss'
+})
+export class DepositCalculatorComponent {
+
+}
